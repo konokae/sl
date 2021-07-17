aa3e2491082d882805dc09b75ec2679b6060f576
konokae <87562298+konokae@users.noreply.github.com>
2021-07-17 16:33:01 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="post1.html"} 
-if(x=="2"){xxx="post2.html"} 
-if(x=="3"){xxx="post3.html"} 
-if(x=="4"){xxx="post4.html"}a_to_ve[i].href="https://jejakngopi.blogspot.com/2018/11"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="cara-membuat-manisan-tomat-yang-maknyus.html"} 
+if(x=="2"){xxx="5-jenis-olahraga-terbaik-menurut-sains.html"} 
+if(x=="3"){xxx="gedhegan-di-ajang-bbf-banyuwangi-batik.html"} 
+if(x=="4"){xxx="festival-ngopi-sepuluh-ewu-banyuwangi.html"}a_to_ve[i].href="https://jejakngopi.blogspot.com/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
